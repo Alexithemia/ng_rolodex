a9f68ec78df0a05dd5f706bd578f646a8f436bf3
Tyler <alexithemia@gmail.com>
2019-02-20 11:36:18 -1000

    Routes work for profile and edit user

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,9 +1,13 @@
 const bookshelf = require('./bookshelf');
-
+const Contact = require('./Contact');
 
 class User extends bookshelf.Model {
   get tableName() { return 'users' }
   get hasTimeStamps() { return true }
+
+  userContacts() {
+    return this.hasMany(Contact, 'created_by');
+  }
 };
 
 module.exports = bookshelf.model('User', User);
---
+++ server/routes/contacts.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const Contact = require('../database/models/Contact')
+const User = require('../database/models/User')
+
+module.exports = router;
