e6473f9b181d0e7ffb9d1fa3d09f534a32050519
Tyler <alexithemia@gmail.com>
2019-02-23 12:34:51 -1000

    Create contact front end validation complete

--- src/app/pages/createContact/createContact.component.ts
+++ src/app/pages/createContact/createContact.component.ts
@@ -31,11 +31,48 @@ export class CreateContactComponent {
       github: ''
     };
 
+  isNameInvalid: boolean = true;
+  isMobileInvalid: boolean = false;
+  isWorkInvalid: boolean = false;
+  isHomeInvalid: boolean = false;
+  isEmailInvalid: boolean = false;
+
   constructor(
     private backend: BackendService,
     private router: Router
   ) {
 
+  }
+
+  validateMobile() {
+    const { mobile } = this.formData;
+    if (mobile.length < 10 && mobile) { this.isMobileInvalid = true }
+    else { this.isMobileInvalid = false }
+  }
+
+  validateWork() {
+    const { work } = this.formData;
+    if (work.length < 10 && work) { this.isWorkInvalid = true }
+    else { this.isWorkInvalid = false }
+  }
+
+  validateHome() {
+    const { home } = this.formData;
+    if (home.length < 10 && home) { this.isHomeInvalid = true }
+    else { this.isHomeInvalid = false }
+  }
+
+  validateEmail() {
+    const { email } = this.formData;
+    if (!email.includes('@') && !email.includes('.') && email) { this.isEmailInvalid = true }
+    else { this.isEmailInvalid = false }
+  }
+
+  validateName() {
+    const { name } = this.formData;
+    if (!name) { this.isNameInvalid = true }
+    else if (name.length < 2) { this.isNameInvalid = true }
+    else { this.isNameInvalid = false }
   }
 
   submitForm() {
